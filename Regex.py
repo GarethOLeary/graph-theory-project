0fd65450ccf0f60eea2c2730ae54afb76d0545a7
Gareth O'Leary <G00375321@gmit.ie>
2021-04-29 20:16:24 +0100

    menu added

--- Regex.py
+++ Regex.py
@@ -53,8 +53,6 @@ def shunt(infix):
 
 # Thompsons Construction
 
-# Thompson's construction.
-
 class State:
     """A state and its arrows in Thompson's construction."""
     def __init__(self, label, arrows, accept):
@@ -191,8 +189,40 @@ def re_to_nfa(postfix):
 
 
 
-if __name__ == "__main__":
-    for postfix in ["abb.*.a.", "100.*.1.", 'ab|']:
-        print(f"postfix: {postfix}")
-        print(f"nfa:     {re_to_nfa(postfix)}")
-        print()
+def print_menu():       ## Your menu design here
+    
+    print ("1. Please Enter infix expression and string")
+    print ("2. Read from file ")
+    print ("3. Exit")
+    
+    
+  
+loop=True      
+        
+while loop:          ## While loop which will keep going until loop = False
+    print_menu()    ## Displays menu
+    choice = input("Enter your choice [1-5]: ")
+            
+    if choice == "1":          
+        print ("Menu 1 has been selected")
+            
+    elif choice== "2":
+        print ("Menu 2 has been selected")
+            
+    elif choice== "3":
+        print ("Menu 3 has been selected")
+                
+        loop=False # This will make the while loop to end as not value of loop is set to False
+    else:
+                
+        input("Wrong option selection. Enter any key to try again..")
+
+        
+
+
+
+#if __name__ == "__main__":
+   # for postfix in ["abb.*.a.", "100.*.1.", 'ab|']:
+       # print(f"postfix: {postfix}")
+       # print(f"nfa:     {re_to_nfa(postfix)}")
+       # print()
